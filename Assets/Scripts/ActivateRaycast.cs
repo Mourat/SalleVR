b0bdec8afce201c41f4a8d3b1647994d566b5ef7
larbo2022 <larefboualem@yahoo.fr>
2023-03-01 16:04:03 +0100

    adding activate Raycast

---
+++ Assets/Scripts/ActivateRaycast.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+
+
+public class ActivateRaycast : MonoBehaviour
+{ 
+  // public GameObject leftTeleportation;
+    // public GameObject rightTeleportation;
+    public GameObject leftHandController;
+    public GameObject rightHandController;
+    public InputActionProperty leftActivate;
+    public InputActionProperty rightActivate;
+  //  public InputActionProperty leftCancel;
+ //  public InputActionProperty rightCancel;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftHandController.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
+        rightHandController.SetActive(rightActivate.action.ReadValue<float>() > 0.1f);
+    }
+}
+
